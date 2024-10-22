6745af84a8bab3ad24607b84e178274b059afa86
Tung Lam Nguyen <tln229@cn65.nauhpc>
2024-10-22 14:56:19 -0700

    done mlp

--- model/mlp/run.sh
+++ model/mlp/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --array=0-1079
-#SBATCH --time=48:00:00
+#SBATCH --array=0-59
+#SBATCH --time=01:00:00
 #SBATCH --mem=16GB
 #SBATCH --cpus-per-task=1
 #SBATCH --output=slurm-out/slurm-%A_%a.out
