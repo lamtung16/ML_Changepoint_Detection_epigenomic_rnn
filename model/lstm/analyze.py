148f57f795d934cfd7d216be3d223d56d0980a08
lamtung16 <nguyenlamtung10@gmail.com>
2024-10-31 10:34:43 -0700

    done lstm

--- model/lstm/analyze.py
+++ model/lstm/analyze.py
@@ -13,17 +13,18 @@ all_dataframes = []
 
 # Loop through each dataset, compression type, and size
 for dataset in datasets:
-    for compress_type in ['mean', 'median']:
-        for compress_size in [100, 1000, 2000]:
-            # Collect all DataFrames from the specified path
-            out_df_list = []
-            for out_csv in glob(f"reports/{dataset}/{compress_type}/{compress_size}/*.csv"):
-                out_df_list.append(pd.read_csv(out_csv))
-           
-            # Concatenate the DataFrames collected in this iteration
-            if out_df_list:  # Check if the list is not empty
-                out_df = pd.concat(out_df_list, ignore_index=True)
-                all_dataframes.append(out_df)  # Append to the master list
+    for loss_type in ['square', 'linear']:
+        for compress_type in ['mean', 'median']:
+            for compress_size in [100, 1000, 2000]:
+                # Collect all DataFrames from the specified path
+                out_df_list = []
+                for out_csv in glob(f"reports/{dataset}/{loss_type}/{compress_type}/{compress_size}/*.csv"):
+                    out_df_list.append(pd.read_csv(out_csv))
+            
+                # Concatenate the DataFrames collected in this iteration
+                if out_df_list:  # Check if the list is not empty
+                    out_df = pd.concat(out_df_list, ignore_index=True)
+                    all_dataframes.append(out_df)  # Append to the master list
 
 # Finally, concatenate all collected DataFrames into one
 if all_dataframes:  # Check if there are any DataFrames to concatenate
